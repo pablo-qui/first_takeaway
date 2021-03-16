715566e465cf9815a7c7cbfd7095bb0400c99a10
pablo-qui <pabloquicios@gmail.com>
2021-03-16 18:32:20 +0100

    final touches

--- app.R
+++ app.R
@@ -191,7 +191,7 @@ server <- function(input, output) {
     
     output$report <- downloadHandler(
         # For PDF output, change this to "report.pdf"
-        filename = "report.html",
+        filename = "report.pdf",
         content = function(file) {
             # Copy the report file to a temporary directory before processing it, in
             # case we don't have write permissions to the current working dir (which
